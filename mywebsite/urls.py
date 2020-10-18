5848ca57c163efb968ab57bf0308c6cd09895c78
fikriazain <fikri.azain@gmail.com>
2020-10-18 20:09:32 +0700

    commit ke 4

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path
 
 from . import views
+from blog import views as blogViews
+from about import views as aboutViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage),
-    path('about/', views.about)
+    path('', views.index),
+    path('blog/', blogViews.index),
+    path('about/', aboutViews.index)
 ]
