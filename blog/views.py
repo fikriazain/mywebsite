265e98f103e78ac8a7286ef9c8c64b4b8aabba19
fikriazain <fikri.azain@gmail.com>
2020-10-18 21:47:27 +0700

    commit ke 5

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 def index(request):
     return render(request, 'blog.html')
+
+def recent(request):
+    return HttpResponse('<h1> Ini adalah recent post </h1>')
