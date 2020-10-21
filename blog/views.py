4f457c0b8613880f863f0893d760d697f64843e9
fikriazain <fikri.azain@gmail.com>
2020-10-21 18:15:35 +0700

    commit lagi

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,12 @@ from django.http import HttpResponse
 def index(request):
     context = {
         'Judul' : 'BLOGKU',
-        'Pembuat' : 'Meh'
+        'Pembuat' : 'Meh',
+        'nav' : [
+            ['/', 'Home'],
+            ['/blog', 'Blog'],
+            ['/about', 'ABOUT']
+        ]
     }
 
     return render(request, 'blog/index.html', context)
--- mywebsite/views.py
+++ mywebsite/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 def index(request):
     context = {
         'Judul' : 'WEBSITE OFFICIAL',
-        'Pembuat' : 'Fikri Aufaa Zain'
+        'Pembuat' : 'Fikri Aufaa Zain',
+        'nav' : [
+            ['/','Home'],
+            ['/blog', 'Blog'],
+            ['/about', 'About']
+        ]
     }
     return render(request, 'index.html', context)
